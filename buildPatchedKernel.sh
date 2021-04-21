3b5ae592d3864ac5590720dd1ab07c83cea8dff5
martinseilerameria <69586698+martinseilerameria@users.noreply.github.com>
2021-04-21 14:59:37 +0200

    Update buildPatchedKernel.sh

--- buildPatchedKernel.sh
+++ buildPatchedKernel.sh
@@ -37,45 +37,6 @@ INSTALL_DIR=$PWD
 # Error out if something goes wrong
 set -e
 
-# Is librealsense on the device?
-
-if [ ! -d "$LIBREALSENSE_DIRECTORY" ] ; then
-   echo "The librealsense repository directory is not available"
-   read -p "Would you like to git clone librealsense? (y/n) " answer
-   case ${answer:0:1} in
-     y|Y )
-         # clone librealsense
-         cd ${HOME}
-         echo "${green}Cloning librealsense${reset}"
-         git clone https://github.com/IntelRealSense/librealsense.git
-         cd librealsense
-         # Checkout version the last tested version of librealsense
-         git checkout $LIBREALSENSE_VERSION
-     ;;
-     * )
-         echo "Kernel patch and build not started"   
-         exit 1
-     ;;
-   esac
-fi
-
-# Is the version of librealsense current enough?
-cd $LIBREALSENSE_DIRECTORY
-VERSION_TAG=$(git tag -l $LIBREALSENSE_VERSION)
-if [ ! $VERSION_TAG  ] ; then
-   echo ""
-  tput setaf 1
-  echo "==== librealsense Version Mismatch! ============="
-  tput sgr0
-  echo ""
-  echo "The installed version of librealsense is not current enough for these scripts."
-  echo "This script needs librealsense tag version: "$LIBREALSENSE_VERSION "but it is not available."
-  echo "This script uses patches from librealsense on the kernel source."
-  echo "Please upgrade librealsense before attempting to patch and build the kernel again."
-  echo ""
-  exit 1
-fi
-
 KERNEL_BUILD_DIR="/usr/src/kernel"
 
 # Get the kernel sources; does not open up editor on .config file
@@ -85,12 +46,12 @@ echo "${green}Patching and configuring kernel${reset}"
 sudo ./scripts/patchKernel.sh
 sudo ./scripts/configureKernel.sh
 
-cd $KERNEL_BUILD_DIR
+# cd $KERNEL_BUILD_DIR
 # Make the new Image and build the modules
 echo "${green}Building Kernel and Modules${reset}"
-./makeKernel.sh
+./scripts/makeKernel.sh
 # Now copy over the built image
-./copyImage.sh
+./scripts/copyzImage.sh
 # Remove buildJetson Kernel scripts
 #if [ $CLEANUP == true ]
 #then
