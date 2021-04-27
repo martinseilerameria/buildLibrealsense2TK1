1b3277f66f5dc3d2fbb565cdd1326b15a232b4f0
martinseilerameria <69586698+martinseilerameria@users.noreply.github.com>
2021-04-27 17:18:44 +0200

    Update installCurrentLibrealsense.sh

--- installCurrentLibrealsense.sh
+++ installCurrentLibrealsense.sh
@@ -15,7 +15,7 @@ sudo apt-get install libglfw3-dev libgtk-3-dev -y
 # make and install
 mkdir build 
 cd build
-~/CMake/bin/cmake ../ -DFORCE_LIBUVC=true -DBUILD_EXAMPLES=true -DCMAKE_BUILD_TYPE=release
+~/RealSense/cmake-3.20.1/bin/cmake ../ -DFORCE_LIBUVC=true -DBUILD_EXAMPLES=true -DCMAKE_BUILD_TYPE=release
 
 NUM_CPU=4
 time make -j$(($NUM_CPU))
