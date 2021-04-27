cd99443f1c10ca32afcd89f886bff322a59a9daa
martinseilerameria <69586698+martinseilerameria@users.noreply.github.com>
2021-04-27 15:15:57 +0200

    Update prepare_system.sh

--- prepare_system.sh
+++ prepare_system.sh
@@ -20,37 +20,14 @@ sudo ln -s /usr/bin/gcc-7 /usr/bin/cc
 sudo ln -s /usr/bin/g++-7 /usr/bin/c++
 
 # install cmake
-cd ${HOME}
-CMAKE_VERSION=3.20.1
-wget https://github.com/Kitware/CMake/archive/v${CMAKE_VERSION}.zip
-# Make it easier to work with a shorter name
-unzip v${CMAKE_VERSION}.zip
-mv CMake-${CMAKE_VERSION} CMake
-rm v${CMAKE_VERSION}.zip
-cd CMake
-time ./bootstrap
-# Get the number of CPUs 
-NUM_CPU=$(nproc)
-time make -j$(($NUM_CPU - 1))
-if [ $? -eq 0 ] ; then
-  echo "CMake make successful"
-else
-  # Try to make again; Sometimes there are issues with the build
-  # because of lack of resources or concurrency issues
-  echo "CMake did not build " >&2
-  echo "Retrying ... "
-  # Single thread this time
-  time make 
-  if [ $? -eq 0 ] ; then
-    echo "CMake make successful"
-  else
-    # Try to make again
-    echo "CMake did not successfully build" >&2
-    echo "Please fix issues and retry build"
-    exit 1
-  fi
-fi
+cd ~
+wget https://github.com/Kitware/CMake/releases/download/v3.20.1/cmake-3.20.1.tar.gz
+tar xf cmake-3.20.1.tar.gz
+cd cmake-3.20.1
+./configure && make
+sudo make install
 
+# swap file
 sudo fallocate -l 4G /swapfile
 sudo chmod 600 /swapfile
 sudo mkswap /swapfile
