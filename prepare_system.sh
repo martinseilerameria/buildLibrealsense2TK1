12edaf6f3a97ae6866fa22ff719a0b2465398f61
martinseilerameria <69586698+martinseilerameria@users.noreply.github.com>
2021-04-28 08:57:14 +0200

    Update prepare_system.sh

--- prepare_system.sh
+++ prepare_system.sh
@@ -39,5 +39,5 @@ sudo fallocate -l 4G /swapfile
 sudo chmod 600 /swapfile
 sudo mkswap /swapfile
 sudo swapon /swapfile
-sudo echo "/swapfile none swap sw 0 0" | tee -a /etc/fstab > /dev/null
+sudo bash -c "printf '/swapfile none swap sw 0 0' >> /etc/fstab"
 sudo reboot
