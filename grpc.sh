66a9704daaa2584d464919086d4ec12be9263620
martinseilerameria <69586698+martinseilerameria@users.noreply.github.com>
2021-04-30 12:18:16 +0200

    Update grpc.sh

--- grpc.sh
+++ grpc.sh
@@ -4,3 +4,17 @@ sudo dd if=/dev/zero of=/swapfile bs=1M count=2048
 sudo mkswap /swapfile
 sudo swapon /swapfile
 sudo reboot
+
+
+sudo apt-get install autoconf libtool
+
+cd ~/RealSense
+git clone -b v1.37.x https://github.com/grpc/grpc
+cd grpc
+git submodule update --init
+
+mkdir -p cmake/build
+cd cmake/build
+~/RealSense/cmake-3.20.1/bin/cmake -DBUILD_SHARED_LIBS=ON -DgRPC_INSTALL=ON -DCMAKE_BUILD_TYPE=Release ../..
+make -j4
+sudo make install
