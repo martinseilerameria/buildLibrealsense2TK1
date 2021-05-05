2347b601a95e05aa0ed85e847261bfcbd26f6137
martinseilerameria <69586698+martinseilerameria@users.noreply.github.com>
2021-05-05 13:22:43 +0200

    Update grpc.sh

--- grpc.sh
+++ grpc.sh
@@ -1,22 +1,20 @@
-sudo swapoff -a
-sudo dd if=/dev/zero of=/swapfile bs=1M count=2048
+#sudo swapoff -a
+#sudo dd if=/dev/zero of=/swapfile bs=1M count=2048
 
-sudo mkswap /swapfile
-sudo swapon /swapfile
-sudo reboot
+#sudo mkswap /swapfile
+#sudo swapon /swapfile
+#sudo reboot
 
 
 sudo apt-get install autoconf libtool
 
-cd ~/RealSense
 git clone -b v1.37.x https://github.com/grpc/grpc
 cd grpc
 git submodule update --init
 
 
-export MY_INSTALL_DIR=$HOME/.local
+export MY_INSTALL_DIR=$CMAKE_PREFIX_PATH
 mkdir -p $MY_INSTALL_DIR
-export PATH="$MY_INSTALL_DIR/bin:$PATH"
 mkdir -p cmake/build
 cd cmake/build
 
@@ -24,18 +22,18 @@ cd cmake/build
 make -j4
 make install
 
-mkdir -p third_party/abseil-cpp/cmake/build
-pushd third_party/abseil-cpp/cmake/build
-cmake -DCMAKE_INSTALL_PREFIX=$MY_INSTALL_DIR \
-      -DCMAKE_POSITION_INDEPENDENT_CODE=TRUE \
-      ../..
-make -j
-make install
+#mkdir -p third_party/abseil-cpp/cmake/build
+#pushd third_party/abseil-cpp/cmake/build
+#cmake -DCMAKE_INSTALL_PREFIX=$MY_INSTALL_DIR \
+#      -DCMAKE_POSITION_INDEPENDENT_CODE=TRUE \
+#      ../..
+#make -j
+#make install
 
-export LD_LIBRARY_PATH=~/.local/lib:$LD_LIBRARY_PATH
+#export LD_LIBRARY_PATH=~/.local/lib:$LD_LIBRARY_PATH
 
 # check if cpp helloworld example compiles
-cd ~/RealSense/grpc/examplescpp/helloworld
+cd ../../grpc/examplescpp/helloworld
 mkdir -p cmake/build
 cd cmake/build
 cmake -DCMAKE_PREFIX_PATH=$MY_INSTALL_DIR ../..
