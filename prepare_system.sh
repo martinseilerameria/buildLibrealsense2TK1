aef774d9a61cfe14142328a9d63573899cd641ed
martinseilerameria <69586698+martinseilerameria@users.noreply.github.com>
2021-05-07 12:17:05 +0200

    Update prepare_system.sh

--- prepare_system.sh
+++ prepare_system.sh
@@ -12,7 +12,8 @@
 # cd RealSense
 # git clone https://github.com/martinseilerameria/buildLibrealsense2TK1.git
 # chmod +x  ./buildLibrealsense2TK1/prepare_system.sh
-# chmod +x  ./buildLibrealsense2TK1/installCurrentLibrealsense.sh
+# chmod +x  ./buildLibrealsense2TK1/install_latest_librealsense.sh
+# chmod +x  ./buildLibrealsense2TK1/install_grpc.sh
 # ./buildLibrealsense2TK1/prepare_system.sh
 
 # update gcc
