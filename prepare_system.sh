b955151c65d32c6baaf2ccf539af7939ba14163d
martinseilerameria <69586698+martinseilerameria@users.noreply.github.com>
2021-05-07 12:14:00 +0200

    Update prepare_system.sh

--- prepare_system.sh
+++ prepare_system.sh
@@ -35,7 +35,7 @@ cd cmake-3.20.1
 sudo make install
 
 # swap file
-sudo fallocate -l 4G /swapfile
+sudo fallocate -l 2G /swapfile
 sudo chmod 600 /swapfile
 sudo mkswap /swapfile
 sudo swapon /swapfile
