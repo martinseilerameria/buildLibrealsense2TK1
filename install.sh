3a817178398a61f11ee6ed84b60891fc31d9ecf1
martinseilerameria <69586698+martinseilerameria@users.noreply.github.com>
2021-05-11 13:43:07 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,14 +14,15 @@ sudo apt-get install libglfw3-dev libgtk-3-dev -y
 
 sudo apt-get install libxinerama-dev -y
 sudo apt-get install libxcursor-dev -y
+sudo apt-get install xorg-dev -y
+sudo apt-get install libglu1-mesa-dev -y
 
 # make and install
 mkdir build 
 cd build
 ~/RealSense/cmake-3.20.1/bin/cmake ../ -DFORCE_LIBUVC=true -DBUILD_EXAMPLES=true -DCMAKE_BUILD_TYPE=release
 
-NUM_CPU=4
-time make -j$(($NUM_CPU))
+make -j4
 sudo make install
 
 
